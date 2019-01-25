ca870be20383586947136d67f8c08009a2ce5a27
Mourya Ruchika <ruchika.mourya@nagra.com>
2019-01-25 17:10:50 +0530

    phone-store-first-commit

---
+++ src/components/cart/EmptyCart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function EmptyCart() {
+  return (
+    <div className = "Container mt-5">
+        <div className = "row">
+        <div className = "col-10 mx-auto text-center text-title">
+        <h1>Yor cart is currently empty</h1>
+        </div>
+        </div>
+    </div>
+  )
+}
